e2fbbee4d7227cb68658af7c39fd9f3016644073
Greshilov V <vgreshilov@mail.ru>
2022-10-03 12:25:34 +0300

    updated: details page comp

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,18 +1,37 @@
+import { useEffect, useMemo, useRef } from 'react';
 import { useParams } from 'react-router-dom';
+import BackButton from '../components/BackButton';
 import MovieDetails from '../components/Movie/Details';
 import NotFound from '../components/NotFound';
+import { useAppDispatch, useAppSelector } from '../hooks';
+import fetchMovieDetails from '../store/features/movie/fetchMovieDetails';
 
 const Details = () => {
-  const { id } = useParams();
+  const dispatch = useAppDispatch();
+  const { id } = useParams<{ id: string }>();
+  const movie = useAppSelector(state => state.movie.current[id as string]);
+  const firstLoadRef = useRef<boolean>(true);
 
-  // todo fetch movie here
-  // const movie =
+  useEffect(() => {
+    const numId = Number(id);
+    if (firstLoadRef.current && !movie && !isNaN(numId)) {
+      dispatch(fetchMovieDetails(Number(id)));
+      firstLoadRef.current = false;
+    }
+  }, [dispatch, id, movie]);
 
-  if (!id || !parseInt(id)) {
-    return <NotFound />;
-  }
+  const pageContent = useMemo(() => {
+    return movie ? <MovieDetails movie={movie} /> : <NotFound />;
+  }, [movie]);
 
-  return <MovieDetails id={id} />;
+  return (
+    <section className="flex flex-col flex-1 text-start overflow-hidden">
+      <div className="absolute p-5" style={{ zIndex: 99 }}>
+        <BackButton />
+      </div>
+      {pageContent}
+    </section>
+  );
 };
 
 export default Details;
