3b71932bd702312b820b4382c751b84361dcb170
Greshilov V <vgreshilov@mail.ru>
2022-10-03 17:30:25 +0300

    refactored: map movies variable in Layout comp

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,18 +6,18 @@ export type TLayoutProps = {
 };
 
 export const Layout = ({ movies }: TLayoutProps) => {
+  const mappedMovies = movies.map(movie => {
+    return (
+      <div key={movie.id} className="w-full p-4 md:w-1/2 lg:w-1/4">
+        <Preview movie={movie} />
+      </div>
+    );
+  });
+
   return (
     <section className="min-h-screen body-font text-gray-600 ">
       <div className="container mx-auto px-5 py-10">
-        <div className="-m-4 flex flex-wrap">
-          {movies.map(movie => {
-            return (
-              <div key={movie.id} className="w-full p-4 md:w-1/2 lg:w-1/4">
-                <Preview movie={movie} />
-              </div>
-            );
-          })}
-        </div>
+        <div className="-m-4 flex flex-wrap">{mappedMovies}</div>
       </div>
     </section>
   );
